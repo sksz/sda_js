54e6f2ac43a1554875d5b010784cf48a769b1b48
Michal Szewczyk <michal.szewczyk@home.net.pl>
2019-12-01 14:19:27 +0100

    js z pliku

---
+++ sda.js
@@ -0,0 +1,11 @@
+var testObject;
+
+testObject = {
+    wartosc1: 'test',
+    wartosc2: 'cos'
+};
+
+delete testObject.wartosc1;
+
+// console.log(testObject);
+document.getElementById('button1').innerHTML = ':P';
