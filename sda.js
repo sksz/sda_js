1e5d94ac55ee1876fc7b6b9534c4cecb154ab7fa
Michal Szewczyk <michal.szewczyk@home.net.pl>
2019-12-07 13:55:22 +0100

    constructor function

--- sda.js
+++ sda.js
@@ -1,24 +1,31 @@
-var myObject, zmienna;
-
-zmienna = 'year';
+var myObject, myObject2, MyObject3;
 
 myObject = new Object();
 myObject.producer = 'Ford';
 myObject.model = 'Mustang';
-myObject['year'] = 1969;
+myObject.year = 1969;
 
-function showProperities(testedObject) {
-    var result = [];
+myObject2 = {
+    producer: 'Ford',
+    model: 'Mustang',
+    year: 1969
+};
 
-    for (var propertyName in testedObject) {
-        // if (testedObject.hasOwnProperty(propertyName)) {
-            result.push(propertyName);
-        // }
+// Constructor
+function MyObject3(producer, model, year) {
+    this.producer = producer;
+    this.model = model;
+    this.year = year;
+    this.speed = 0;
+    this.accelerate = function (accelerateDelta) {
+        this.speed += accelerateDelta;
     }
-
-    return result;
 }
 
-console.log(showProperities(myObject));
-console.log(Object.getOwnPropertyNames(myObject));
-console.log(Object.keys(myObject));
+console.log(myObject);
+console.log(myObject2);
+var test1 = new MyObject3('aaa', 'ssss', 1234);
+console.log(new MyObject3('Ford', 'Mustang', 1969));
+console.log(test1);
+test1.accelerate(1);
+console.log(test1);
