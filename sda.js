1b694773171d07f284250f580482a2ebc948b01d
Michal Szewczyk <michal.szewczyk@home.net.pl>
2019-12-07 13:38:09 +0100

    wlasciwosci - listowanie 1

--- sda.js
+++ sda.js
@@ -7,16 +7,5 @@ myObject.producer = 'Ford';
 myObject.model = 'Mustang';
 myObject['year'] = 1969;
 
-function power(base, index) {
-    if (index < 0) {
-        return;
-    }
-    
-    if (!index) {
-        return 1;
-    }
+console.log(Object.keys(myObject));
 
-    return base * power(base, index - 1);
-}
-
-console.log(power(2, 3));
