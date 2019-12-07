f8cd242354c5399114dca406c3c37b4bda47841c
Michal Szewczyk <michal.szewczyk@home.net.pl>
2019-12-07 09:42:05 +0100

    wyszukiwanie

--- sda.js
+++ sda.js
@@ -1,6 +1,10 @@
 var haystack, needle;
 
-haystack = 'Ala na kota z wasami';
-needle = '.';
+haystack = 'Ala na kota z wasami, kot lubi Alę.';
+needle = 'kot';
 
-document.getElementById('button1').innerHTML = haystack + needle + ' asd';
+document.getElementById('button1').innerHTML = haystack.includes(needle);
+
+document.getElementById('button1').innerHTML += ' -first- ' + haystack.indexOf(needle);
+
+document.getElementById('button1').innerHTML += ' -last- ' + haystack.lastIndexOf(needle);
