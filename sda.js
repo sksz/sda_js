1540208233fa6c235c98fc66ba22c114fc58fdb4
Michal Szewczyk <michal.szewczyk@home.net.pl>
2019-12-07 14:39:24 +0100

    getter & setter

--- sda.js
+++ sda.js
@@ -1,31 +1,23 @@
-var myObject, myObject2, MyObject3;
-
-myObject = new Object();
-myObject.producer = 'Ford';
-myObject.model = 'Mustang';
-myObject.year = 1969;
-
-myObject2 = {
-    producer: 'Ford',
-    model: 'Mustang',
-    year: 1969
-};
+var instance, MyObject;
 
 //Constructor
-function MyObject3(producer, model, year) {
-    this.producer = producer;
-    this.model = model;
-    this.year = year;
-    this.speed = 0;
-    this.accelerate = function (accelerateDelta) {
-        this.speed += accelerateDelta;
+MyObject = {
+    producer: '',
+    model: '',
+    year: '',
+    mySpeed: 0,
+
+    get speed () {
+        return this.mySpeed + 1;
+    },
+
+    set speed (newSpeed) {
+        this.mySpeed = newSpeed;
     }
 }
 
-console.log(myObject);
-console.log(myObject2);
-var test1 = new MyObject3('aaa', 'ssss', 1234);
-console.log(new MyObject3('Ford', 'Mustang', 1969));
-console.log(test1);
-test1.accelerate(1);
-console.log(test1);
+MyObject.speed = 2;
+console.log(MyObject);
+console.log(MyObject.speed);
+MyObject.mySpeed = 1;
+console.log(MyObject.speed);
