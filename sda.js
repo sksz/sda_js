52489a2dd359d545fae584bbec4de7efa822ff28
Michal Szewczyk <michal.szewczyk@home.net.pl>
2019-12-07 11:11:06 +0100

    poprawione koty

--- sda.js
+++ sda.js
@@ -1,21 +1,35 @@
 var haystack, needle;
 
-haystack = 'Ala na kota z wasami, kot lubi Alę. Czy Ala i Kot to istoty?';
+haystack = 'kot Ala na kota z wasami, kot lubi Alę. Czy Ala i Kot to istoty?';
 needle = 'kot';
 
 function szukamKota() {
     var pozycjaKota, koty;
+
+    if (needle === '') {
+        return [];
+    }
+
     pozycjaKota = 0;
     koty = [];
 
     while (pozycjaKota !== -1) {
-        pozycjaKota = haystack.indexOf(needle, pozycjaKota + 1);
+        pozycjaKota = haystack.indexOf(needle, pozycjaKota);
         if (pozycjaKota >= 0) {
             koty.push(pozycjaKota);
+            pozycjaKota++;
         }
     }
 
     return koty;
 }
 
-document.getElementById('button1').innerHTML = szukamKota();
+function kociakowanie() {
+    var koty;
+
+    koty = szukamKota();
+
+    return koty.length > 0 ? koty : 'Nie ma kotów';
+}
+
+document.getElementById('button1').innerHTML = kociakowanie();
