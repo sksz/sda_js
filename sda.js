ba5f936533c751722d0851f457a3d1cf7872d40e
Michal Szewczyk <michal.szewczyk@home.net.pl>
2019-12-08 09:30:53 +0100

    element po id , ukrywanie i usuwanie

--- sda.js
+++ sda.js
@@ -2,4 +2,7 @@ var init;
 
 init = function() {
     console.log('Hello');
+    document.getElementById('button1').hidden = true;
+    // document.getElementById('button1').remove();
+    console.log(document.getElementById('button1'));
 }
