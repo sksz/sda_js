fe409973ae20be2a8928728d509b7d70daa586c9
Michal Szewczyk <michal.szewczyk@home.net.pl>
2019-12-08 09:19:47 +0100

    dom - part 1

--- sda.js
+++ sda.js
@@ -1,29 +1,5 @@
-var Person, test, Teacher;
+var init;
 
-test = 'aaa';
-
-function Person(first, last, age, eyecolor) {
-    this.firstName = first;
-    this.lastName = last;
-    this.age = age;
-    this.eyeColor = eyecolor;
+init = function() {
+    console.log('Hello');
 }
-
-Person.prototype.nationality = `English ${test}`;
-var instance = new Person('Ala', 'Nijaka', 12, 'black');
-
-console.log(instance);
-console.log(instance.nationality);
-
-function Teacher(first, last, age, eyeColor, interests) {
-    Person.call(this, first, last, age, eyeColor);
-
-    this.interests = interests;
-}
-
-Teacher.prototype = Person.prototype;
-
-var teach = new Teacher('Jan', 'Kowalski', 920, 'white', 'vodka');
-
-console.log(teach);
-console.log(teach.nationality);
