9d94b34466e93a0225cd5b24f5d2d9f6deebdbd1
Michal Szewczyk <michal.szewczyk@home.net.pl>
2019-12-08 14:08:41 +0100

    wstęp do struktury

--- sda.js
+++ sda.js
@@ -1,4 +1,51 @@
 var init;
 
 init = function() {
+    var user, book, lend;
+
+    user = [
+        {
+            id: 1,
+            login: 'jan.kowalski',
+            password: '******',
+            email: 'jan.kowalski@nijaka.pl',
+            avatar: 'jan.kowalski.jpg',
+            roles: [
+                {
+                    accountRole: 'administrator'
+                }, {
+                    accountRole: 'user'
+                }
+            ],
+            personalData: {
+                firstName: 'Jan',
+                lastName: 'Nijaki',
+                birthDate: '01.01.-20',
+                gender: 'M',
+                phoneNumber: '+48914112233'
+            }
+        }
+    ];
+
+    book = [
+        {
+            isdn: '123123-qdsdsad-123123',
+            title: 'ANSI C',
+            authors: 'Kerrigan, Ritche',
+            publishYear: 1981,
+            publisher: 'Ja :)'
+        }
+    ];
+
+    lend = [
+        {
+            lendDate: '07-12-2019',
+            lendTo: '07-01-2020',
+            book: '123123-qdsdsad-123123',
+            user: 1,
+            deleted: false
+        }
+    ];
+
+    
 }
