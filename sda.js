40e0593e325fab01e57264ee254a8f2290803b76
Michal Szewczyk <michal.szewczyk@home.net.pl>
2019-12-08 09:38:42 +0100

    Dodawanie elementów

--- sda.js
+++ sda.js
@@ -1,8 +1,15 @@
 var init;
 
 init = function() {
+    var heading, heading_text;
+
     console.log('Hello');
-    document.getElementById('button1').hidden = true;
+    // document.getElementById('button1').hidden = true;
     // document.getElementById('button1').remove();
-    console.log(document.getElementById('button1'));
+    // console.log(document.getElementById('button1'));
+
+    heading = document.createElement('h1');
+    heading_text = document.createTextNode('Big Head!');
+    heading.appendChild(heading_text);
+    document.body.appendChild(heading);
 }
