228758c5cca36f92b3e48a3719425d560a92c38b
Michal Szewczyk <michal.szewczyk@home.net.pl>
2020-01-11 11:37:42 +0100

    Insertion Sort

--- sda.js
+++ sda.js
@@ -1,58 +1,23 @@
-var init;
+let arr = [12, 55, 7];
 
-init = function() {
-    class HashNode {
-        constructor(key, value) {
-            this.key = key;
-            this.value = value;
+function insertSort(arr) {
+    var element, i;
+    for(i = 0; i < arr.length; i++) {
+        element = arr[i];
+
+        if(arr[i] > arr[i - 1]) {
+            continue;
+        } else {
+            var j;
+            for(j = i - 1; j >= 0 && arr[j] > element; j--) {
+                arr[j + 1] = arr[j];
+            }
+
+            arr[j + 1] = element;
         }
     }
 
-    class HashTable {
-        constructor() {
-            this.cells = new Array(0);
-        }
-
-        getHash(key) {
-            for (let i = 0; i < this.cells.length; i++) {
-                if (this.cells[i].key === key) {
-                    return this.cells[i].value;
-                }
-            }
-        }
-
-        push(hashNode) {
-            if (typeof this.getHash(hashNode.key) !== 'undefined') {
-                return;
-            }
-
-            this.cells.push(hashNode);
-        }
-
-        pop() {
-            if (this.cells.length === 0) {
-                return;
-            }
-
-            let element = this.cells[0];
-            let tempArray = new Array(this.cells.length - 1);
-
-            for (var i = 1; i < this.cells.length; i++) {
-                tempArray[i - 1] = this.cells[i];
-            }
-
-            this.cells = tempArray;
-            return element;
-        }
-    }
-
-    var tablica = new HashTable();
-
-    tablica.push(new HashNode('key1', 'value1'));
-    tablica.push(new HashNode('key2', 'value2'));
-    tablica.push(new HashNode('key1', 'value3'));
-
-    console.log(tablica.pop());
-
-    console.log(tablica);
+    return arr;
 }
+
+console.log(insertSort(arr));
