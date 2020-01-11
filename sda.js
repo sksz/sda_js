16dce86d9e19c074ad8a0896b1ea570dc876acae
Michal Szewczyk <michal.szewczyk@home.net.pl>
2020-01-11 12:04:50 +0100

    quick sort

--- sda.js
+++ sda.js
@@ -1,23 +1,55 @@
-let arr = [12, 55, 7];
+let array = [12, 55, 7, 54, 22];
 
-function insertSort(arr) {
-    var element, i;
-    for(i = 0; i < arr.length; i++) {
-        element = arr[i];
+function quickSort(array, left, right) {
+    var index, left, right;
 
-        if(arr[i] > arr[i - 1]) {
-            continue;
-        } else {
-            var j;
-            for(j = i - 1; j >= 0 && arr[j] > element; j--) {
-                arr[j + 1] = arr[j];
-            }
+    if(array.length < 2) {
+        return array;
+    }
 
-            arr[j + 1] = element;
+    index = partition(array, left, right);
+    if (index - 1 > left) {
+        quickSort(array, left, index - 1);
+    }
+
+    if (index < right) {
+        quickSort(array, index, right);
+    }
+
+    return array;
+}
+
+function partition (array, left, right) {
+    var pivot;
+    pivot = array[Math.floor((right + left) / 2)];
+    i = left;
+    j = right;
+
+    while (i <= j) {
+        while (array[i] < pivot) {
+            i++;
+        }
+
+        while (array[j] > pivot) {
+            j--;
+        }
+
+        if (i <= j) {
+            swap(array, i, j);
+            i++;
+            j--;
         }
     }
 
-    return arr;
+    return i;
 }
 
-console.log(insertSort(arr));
+function swap(array, leftIndex, rightIndex) {
+    var temp;
+
+    temp = array[leftIndex];
+    array[leftIndex] = array[rightIndex];
+    array[rightIndex] = temp;
+}
+
+console.log(quickSort(array, 0, array.length - 1));
