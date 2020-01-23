fe10b0d1c903f4a3d256da46b2dcedb1d92b1ec6
nwolf14 <nwolf959@gmail.com>
2020-01-23 20:10:16 +0100

    small update

--- tasks/additional-examples/8. Async code - test/main.js
+++ tasks/additional-examples/8. Async code - test/main.js
@@ -15,4 +15,7 @@ test();
 console.log('8');
 
 // typy kodu asynchronicznego w JavaScript: setTimeout/setInterval, obsługa zdarzeń, AJAX, Promise/async
-// w tym przypadku kod asynchroniczny w setTimeout() zostanie wykonany dopiero wtedy kiedy zakończymy wykonywanie kodu synchronicznego
+
+// w tym przypadku kod asynchroniczny w setTimeout() zostanie wykonany dopiero wtedy kiedy zakończymy wykonywanie kodu synchronicznego, 
+// jest to związane z event loop która jest podstawą działania silnika JavaScript, polecam wam ten film wyjaśniający działanie pętli zdarzeń:
+// https://www.youtube.com/watch?v=cCOL7MC4Pl0&t=1s
