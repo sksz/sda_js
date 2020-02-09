2db1b80f7cd63f662727a11f7db82e05bc571276
Michał Szewczyk <michal.szewczyk@home.net.pl>
2020-02-09 12:15:02 +0100

    podwojny cydzyslow

--- php/hello.php
+++ php/hello.php
@@ -8,6 +8,17 @@ To jest część poza PHP.
  * Komentarz wieloliniowy (Multi-Line)
  */
 echo "Goodbye, world!\n\n";
+echo 'Oto tekst
+zlozony z kilku linii.';
+
+// prefiksowanie cydzysłowów
+echo "aaa\b\taaa\n ' \"\n";
+echo 'Dodajemy \'apostrof\' do stringa.';
+echo "\n\n";
+
+$zmienna = 'test';
+
+echo "Zmienna to $zmienna\n\n";
 
 // Komentarz w stylu C++
 // include('./testImport.php');
