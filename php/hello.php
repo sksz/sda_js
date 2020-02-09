d94a61fc8f12fd313984d5ff90818702fdf9925f
Michał Szewczyk <michal.szewczyk@home.net.pl>
2020-02-09 10:49:19 +0100

    Dołączanie plików

--- php/hello.php
+++ php/hello.php
@@ -4,4 +4,12 @@
 
 To jest część poza PHP.
 <?php
-echo "Goodbye, world!";
+echo "Goodbye, world!\n\n";
+
+// include('./testImport.php');
+include_once('./testImport.php');
+include_once('./testImport.php');
+
+// require('testImport2.php');
+require_once('testImport2.php');
+require_once('testImport2.php');
---
+++ php/testImport2.php
@@ -0,0 +1,6 @@
+<?php
+
+    echo 'test2';
+    const B = 'b';
+
+?>
