d3e227ae522385c215072db15abe06f4d09e6cee
Michał Szewczyk <michal.szewczyk@home.net.pl>
2020-02-15 12:19:03 +0100

    dni tygodnia

--- php/zad7.php
+++ php/zad7.php
@@ -1,4 +1,48 @@
 <?php
-$var = ['wl1' => 5, 'wl2' => 'test'];
-$cast = (object) $var;
-var_dump($var, $cast);
+
+$checkWeekday = 'Sunday';
+
+$weekDays = [
+    'Sunday' => [
+        'pl' => 'Niedziela',
+        'de' => 'Sonntag',
+    ],
+    'Monday' => [
+        'pl' => 'Poniedziałek',
+        'de' => 'Montag',
+    ],
+    'Tuesday' => [
+        'pl' => 'Wtorek',
+        'de' => 'Dienstag',
+    ],
+    'Wednesday' => [
+        'pl' => 'Środa',
+        'de' => 'Mittwoch',
+    ],
+    'Thursday' => [
+        'pl' => 'Czwartek',
+        'de' => 'Donnerstag',
+    ],
+    'Friday' => [
+        'pl' => 'Piątek',
+        'de' => 'Freitag',
+    ],
+    'Saturday' => [
+        'pl' => 'Sobota',
+        'de' => 'Samstag',
+    ],
+];
+
+$language = [
+    'pl' => 'polskim',
+    'de' => 'niemieckim',
+    'it' => 'włoskim',
+];
+
+foreach ($language as $key => $value) {
+    echo 'Dzień ' . $checkWeekday . ' ' . (isset($weekDays[$checkWeekday][$key])  ? 'ma tłumaczenie ' : 'nie ma tłumaczenia ') . 'w języku ' . $value, PHP_EOL;
+}
+
+
+
+
