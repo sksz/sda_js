2ffd48e20a5e2086f499d8e41bd3d30eb605c9b3
Michał Szewczyk <michal.szewczyk@home.net.pl>
2020-05-17 13:18:04 +0200

    Udoskonalony file cache

--- php/redis/fileCache.php
+++ php/redis/fileCache.php
@@ -2,9 +2,11 @@
 
 class FileCache
 {
-    const DIR = 'cache/';
+    private static $directory = 'cache/';
 
-    static $instance;
+    private static $instance;
+
+    private static $ttl = 3600;
 
     /**
      * @return DivanteCacheManager
@@ -16,15 +18,27 @@ class FileCache
         return self::$instance = new self;
     }
 
+    public static function setTtl($ttl)
+    {
+        self::$ttl = $ttl;
+    }
+
+    public static function setDirectory($directory)
+    {
+        self::$directory = $directory;
+    }
+
      /**
       * @param string $key
       * @param mixed $data
       */
-    public function save($key, $data, $ttl)
+    public function save($key, $data, $ttl = -1)
     {
+        if ($ttl < 0) $ttl = self::$ttl;
+
         if (!$this->keyExists($key, $ttl)) {
-            $fileObject = new SplFileObject(self::DIR . $key, 'w');
-            $fileObject->fwrite($data);
+            $fileObject = new SplFileObject(self::$directory . $key, 'w');
+            $fileObject->fwrite(serialize($data));
         }
     }
 
@@ -32,10 +46,12 @@ class FileCache
      * @param string $key
      * @return mixed
      */
-    public function load($key, $ttl)
+    public function load($key, $ttl = -1)
     {
+        if ($ttl < 0) $ttl = self::$ttl;
+
         if ($this->keyExists($key, $ttl)) {
-            $fileObject = new SplFileObject(self::DIR . $key);
+            $fileObject = new SplFileObject(self::$directory . $key);
             $tmp = '';
 
             while (!$fileObject->eof()) {
@@ -43,7 +59,7 @@ class FileCache
                 $fileObject->next();
             }
 
-            return $tmp;
+            return unserialize($tmp);
         }else{
             return false;
         }
@@ -61,14 +77,14 @@ class FileCache
      * @param string $key
      * @return bool
      */
-    protected function keyExists($key, $ttl)
+    protected function keyExists($key, $ttl = -1)
     {
-        if (file_exists(self::DIR . $key)) {
-            $fileInfoObject = new SplFileInfo(self::DIR . $key);
+        if ($ttl < 0) $ttl = self::$ttl;
+
+        if (file_exists(self::$directory . $key)) {
+            $fileInfoObject = new SplFileInfo(self::$directory . $key);
             $fileModifyTime = $fileInfoObject->getMTime();
             $time = time() - $fileModifyTime;
-
-            var_dump($time < $ttl);
 
             return $time < $ttl;
         };
@@ -77,7 +93,11 @@ class FileCache
     }
 }
 var_dump(FileCache::getInstance()->load('klucz1', 3600));
-
 FileCache::getInstance()->save('klucz1', 'qwe', 20);
-
 var_dump(FileCache::getInstance()->load('klucz1', 200));
+
+FileCache::getInstance()->save('klucz2', new StdClass());
+var_dump(FileCache::getInstance()->load('klucz2'));
+
+FileCache::getInstance()->save('klucz3', ['key1' => 'val1', 'key2' => 'val2', 'test']);
+var_dump(FileCache::getInstance()->load('klucz3'));
