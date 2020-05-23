04d9ae2e8c62e15e5cb6e475e726ec96085e8b85
Michał Szewczyk <michal.szewczyk@home.net.pl>
2020-05-23 10:05:53 +0200

    Bootstrap + cli-config

---
+++ php/Doctrine/bootstrap.php
@@ -0,0 +1,29 @@
+<?php
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+require_once "vendor/autoload.php";
+
+$isDevMode = true;
+$proxyDir = null;
+$cache = null;
+$useSimpleAnnotationReader = false;
+
+$config = Setup::createAnnotationMetadataConfiguration(
+    [__DIR__."/src"],
+    $isDevMode,
+    $proxyDir,
+    $cache,
+    $useSimpleAnnotationReader
+);
+
+$conn = [
+    'dbname' => 'bugtracker',
+    'user' => 'root',
+    'password' => '1qwasa',
+    'host' => 'localhost',
+    'driver' => 'pdo_mysql',
+];
+
+$entityManager = EntityManager::create($conn, $config);
