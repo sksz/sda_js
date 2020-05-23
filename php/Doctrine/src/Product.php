9f09c9d33bf294d496c1aa49215a07f6fa23c623
Michał Szewczyk <michal.szewczyk@home.net.pl>
2020-05-23 10:36:44 +0200

    Encja produktu

---
+++ php/Doctrine/src/Product.php
@@ -0,0 +1,40 @@
+<?php
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="products")
+ */
+class Product
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    protected $name;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
