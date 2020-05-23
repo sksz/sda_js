6d14d42b99f9a18f63d2c19d053dcc9c8831d226
Michał Szewczyk <michal.szewczyk@home.net.pl>
2020-05-23 11:28:09 +0200

    Wybranie danych part 2

--- php/Doctrine/show_product.php
+++ php/Doctrine/show_product.php
@@ -3,7 +3,13 @@
 require_once "bootstrap.php";
 
 $id = $argv[1];
-$product = $entityManager->find('Product', $id);
+
+// Użycie bezpośrednio EM do wybrania danych bez pobierania repozytorium
+// $product = $entityManager->find('Product', $id);
+
+// Pobranie repozytorium z EM i użycie go do wybrania danych
+$productRepository = $entityManager->getRepository('Product');
+$product = $productRepository->find($id);
 
 if ($product === null) {
     echo "No product found.\n";
